63db6e470862b928167d56a444b61ff5183564ca
FREDY ANTONIO MAZO QUICENO <frema1914@gmail.com>
2023-11-12 06:38:15 -0500

    Project Created in ASP.NET.Core

---
+++ Parcial3_MazoQuicenoFredyAntonio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Parcial3_MazoQuicenoFredyAntonio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
